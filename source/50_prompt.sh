dd7c5b0cbdbb1de2743831699235b3ac07291503
Ionut Colceriu <contact@ghinda.net>
2016-12-15 17:03:14 +0200

    bash prompt tweaks. `git up` alias. byzanz-record-region tweaks.

--- source/50_prompt.sh
+++ source/50_prompt.sh
@@ -1,11 +1,12 @@
 # My awesome bash prompt
 #
+# Forked from:
 # Copyright (c) 2012 "Cowboy" Ben Alman
 # Licensed under the MIT license.
 # http://benalman.com/about/license/
 #
 # Example:
-# [master:!?][cowboy@CowBook:~/.dotfiles]
+# [master:!?][~/.dotfiles]
 # [11:14:45] $
 #
 # Read more (and see a screenshot) in the "Prompt" section of
@@ -94,7 +95,8 @@ function prompt_command() {
   # misc: [cmd#:hist#]
   # PS1="$PS1$c1[$c0#\#$c1:$c0!\!$c1]$c9"
   # path: [user@host:path]
-  PS1="$PS1$c1[$c0\u$c1@$c0\h$c1:$c0\w$c1]$c9"
+  #PS1="$PS1$c1[$c0\u$c1@$c0\h$c1:$c0\w$c1]$c9"
+  PS1="$PS1$c1[$c0\w$c1]$c9"
   PS1="$PS1\n"
   # date: [HH:MM:SS]
   PS1="$PS1$c1[$c0$(date +"%H$c1:$c0%M$c1:$c0%S")$c1]$c9"
