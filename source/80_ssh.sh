25f2e6e3f239c9311fa276753152017371de981a
Ionut Colceriu <contact@ghinda.net>
2017-10-05 10:18:46 -0700

    ssh autostart tweaks

--- source/80_ssh.sh
+++ source/80_ssh.sh
@@ -1,11 +1,8 @@
 export SSH_ASKPASS=ksshaskpass
+if ! pgrep -u "$USER" ssh-agent > /dev/null; then
+    ssh-agent > ~/.ssh-agent-thing
+fi
+if [[ "$SSH_AGENT_PID" == "" ]]; then
+    eval "$(<~/.ssh-agent-thing)"
+fi
 
-{
-  if ! pgrep -u $USER ssh-agent > /dev/null; then
-      ssh-agent > ~/.ssh-agent-thing
-  fi
-  if [[ "$SSH_AGENT_PID" == "" ]]; then
-      eval $(<~/.ssh-agent-thing) > /dev/null
-  fi
-  ssh-add </dev/null
-} &> /dev/null
