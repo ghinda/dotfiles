046292d76adb32712d6d7b867137feddc35b4d1f
Ionut Colceriu <contact@ghinda.net>
2018-02-22 11:12:01 +0200

    just use keychain.

--- source/80_ssh.sh
+++ source/80_ssh.sh
@@ -1,8 +1,1 @@
-export SSH_ASKPASS=ksshaskpass
-if ! pgrep -u "$USER" ssh-agent > /dev/null; then
-    ssh-agent > ~/.ssh-agent-thing
-fi
-if [[ "$SSH_AGENT_PID" == "" ]]; then
-    eval "$(<~/.ssh-agent-thing)"
-fi
-
+eval `SSH_ASKPASS=ksshaskpass keychain --eval --quiet --quick --agents ssh id_rsa`
